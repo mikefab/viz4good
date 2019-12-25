6c8784a356f30434b1de2b9c7a0f7369d2b9223c
mikefab <mikefabrikant@gmail.com>
2019-12-25 19:56:19 +0000

    dialog

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
+import Dialog, {
+  DialogTitle,
+  DialogContent,
+  DialogFooter,
+  DialogButton,
+} from '@material/react-dialog';
 import App1 from './components/demo-app2/src/app';
 
 class App extends Component {
   state = {
+    isOpen: false,
     keplerJson: null,
     created_at: null,
     fetched_at: null,
@@ -57,8 +64,14 @@ class App extends Component {
       this._handleKeplerJson(keplerJson, this, 'refresh')
     })
   }
-  render() {
 
+  _handleDialog = () => {
+    this.setState({
+      isOpen: this.state.isOpen ? false : true
+    })
+  }
+  render() {
+        let isOpen = this.state.isOpen
         let keplerJson = this.state.keplerJson
         let created_at = new Date(this.state.created_at).toLocaleDateString('en-US')
         let refreshed_at = this.state.refreshed_at
@@ -68,15 +81,45 @@ class App extends Component {
           <div >
             <div className="mdc-layout-grid"id='intro' >
                 This is an animated visualization of Ebola confirmed cases in the North Kivu Ebola Outbreak in the Democratic Republic of the Congo (DRC).
-                &nbsp;Here is the <a href='https://github.com/mikefab/viz4good'>code base</a> and <a href='https://data.humdata.org/dataset/ebola-cases-and-deaths-drc-north-kivu'>border file</a>.
-              &nbsp;The data was last fetched from <a href='https://data.humdata.org/dataset/ebola-cases-and-deaths-drc-north-kivu'>The Humanitarian Data Exchanage</a> on {created_at}.&nbsp;
-              <button className="mdc-button" onClick={this._refreshData}>
-                <div className="mdc-button__ripple"></div>
-                <span className="mdc-button__label">Click to fetch latest version</span>
-              </button>
-              &nbsp; {refreshed_at}
-
+                <button className="mdc-button" onClick={this._handleDialog}>
+                  <div className="mdc-button__ripple"></div>
+                  <span className="mdc-button__label">Click here for more on this app</span>
+                </button>
+              <Dialog open={isOpen}>
+                <DialogTitle>Ebola confirmed cases visualization</DialogTitle>
+                <DialogContent className='mdc-dialog--scrollable'>
+                  The data displayed in this map was last fetched from <a target='_blank' href='https://data.humdata.org/dataset/ebola-cases-and-deaths-drc-north-kivu'>The Humanitarian Data Exchange</a> on <strong>{created_at}</strong>. They update it every few days.
+                  Click
+                  <button className="mdc-button" onClick={this._refreshData}>
+                    <div className="mdc-button__ripple"></div>
+                    <span className="mdc-button__label"> here </span>
+                  </button>
+                  to fetch latest data.
+                  <div style={{height: '15px'}}>
+                    <strong>
+                      {refreshed_at}
+                    </strong>
+                  </div>
+                  <br/>
+                  The code for this app can be found <a target='_blank' href='https://github.com/mikefab/viz4good'>here</a>. The borderfile is <a target='_blank' href='https://data.humdata.org/dataset/ebola-cases-and-deaths-drc-north-kivu'>here</a>.
+                <br/>
+                <h3>Pro tips</h3>
+                <ul>
+                  <li>
+                    The map might not show up on some android devices.
+                  </li>
+                  <li>
+                    You'll have to zoom out a bit to see all areas with cases.
+                  </li>
+                </ul>
+                <img src='/images/Ebola_Confirmed_Cases__DRC_.jpg' width='400'/>
+                </DialogContent>
+                <DialogFooter>
+                  <DialogButton onClick={this._handleDialog}>Close</DialogButton>
+                </DialogFooter>
+              </Dialog>
             </div>
+
             <App1 keplerJson={keplerJson}  introHeight={introHeight}/>
           </div>
         );
