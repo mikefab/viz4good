6b58bae41c18cb80ab7092522472b9035322314f
mikefab <mikefabrikant@gmail.com>
2019-12-26 13:40:07 +0000

    Content cleanup

--- client/src/app.js
+++ client/src/app.js
@@ -80,16 +80,16 @@ class App extends Component {
         templates.push(
           <div >
             <div className="mdc-layout-grid"id='intro' >
-                This is an animated visualization of Ebola confirmed cases in the North Kivu Ebola Outbreak in the Democratic Republic of the Congo (DRC).
+                This is an animation of Ebola confirmed cases in the North Kivu Ebola Outbreak in the Democratic Republic of the Congo.
                 <button className="mdc-button" onClick={this._handleDialog}>
                   <div className="mdc-button__ripple"></div>
-                  <span className="mdc-button__label">Click here for more on this app</span>
+                  <span className="mdc-button__label">Click here for a guide to the app</span>
                 </button>
               <Dialog open={isOpen}>
                 <DialogTitle>Ebola confirmed cases visualization</DialogTitle>
                 <DialogContent className='mdc-dialog--scrollable'>
 		  <br/>
-                  <div>
+                  <div style={{fontSize: '16px'}}>
                     The data displayed in this map was last fetched from <a target='_blank' href='https://data.humdata.org/dataset/ebola-cases-and-deaths-drc-north-kivu'>The Humanitarian Data Exchange</a> on <strong>{created_at}</strong>. They update it every few days.
                     Click
                     <button className="mdc-button" onClick={this._refreshData}>
